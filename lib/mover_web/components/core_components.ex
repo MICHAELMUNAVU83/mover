ad3c7abd8b7d43d7ef3af4607d0c4802fdffe023
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-09-12 17:46:38 +0300

    Added subquery on ex file

--- lib/mover_web/components/core_components.ex
+++ lib/mover_web/components/core_components.ex
@@ -273,6 +273,7 @@ defmodule MoverWeb.CoreComponents do
   attr :name, :any
   attr :label, :string, default: nil
   attr :value, :any
+  attr :class, :string, default: nil
 
   attr :type, :string,
     default: "text",
@@ -371,14 +372,16 @@ defmodule MoverWeb.CoreComponents do
     ~H"""
     <div phx-feedback-for={@name}>
       <.label for={@id}><%= @label %></.label>
+
       <input
         type={@type}
         name={@name}
         id={@id}
         value={Phoenix.HTML.Form.normalize_value(@type, @value)}
         class={[
-          "mt-2 block w-full rounded-lg text-zinc-900 focus:ring-0 sm:text-sm sm:leading-6",
+          "mt-2 block w-full bg-red-50 rounded-lg text-zinc-900 focus:ring-0 sm:text-sm sm:leading-6",
           "phx-no-feedback:border-zinc-300 phx-no-feedback:focus:border-zinc-400",
+          @class,
           @errors == [] && "border-zinc-300 focus:border-zinc-400",
           @errors != [] && "border-rose-400 focus:border-rose-400"
         ]}

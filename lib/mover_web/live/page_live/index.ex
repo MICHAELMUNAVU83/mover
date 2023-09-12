710a9ce8734649fa014e1735443a948e4e358a70
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-09-12 18:20:21 +0300

    Added subquery on ex file

--- lib/mover_web/live/page_live/index.ex
+++ lib/mover_web/live/page_live/index.ex
@@ -8,10 +8,36 @@ defmodule MoverWeb.PageLive.Index do
 
     {:ok,
      socket
+     |> assign(quote: %Quote{})
      |> assign_form(changeset)}
   end
 
   defp assign_form(socket, %Ecto.Changeset{} = changeset) do
     assign(socket, :form, to_form(changeset))
   end
+
+  def handle_event("validate", %{"quote" => quote_params}, socket) do
+    changeset =
+      socket.assigns.quote
+      |> Quotes.change_quote(quote_params)
+      |> Map.put(:action, :validate)
+
+    {:noreply, assign_form(socket, changeset)}
+  end
+
+  def handle_event("save", %{"quote" => quote_params}, socket) do
+    case Quotes.create_quote(quote_params) do
+      {:ok, quote} ->
+        notify_parent({:saved, quote})
+
+        {:noreply,
+         socket
+         |> put_flash(:info, "Quote created successfully")}
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        {:noreply, assign_form(socket, changeset)}
+    end
+  end
+
+  defp notify_parent(msg), do: send(self(), {__MODULE__, msg})
 end
